01d192c4be3cc6c94c9c39c14da0bdd476146a52
yodavo <davidguevaraapaza@gmail.com>
2021-08-03 10:36:10 -0500

    Intro David in Julia Language

---
+++ David_stage1.jl
@@ -0,0 +1,15 @@
+#!usr/bin/julia
+
+########### HACKBIO 2021 ###########
+# Stage 0 - Introduction
+
+name = "David Guevara-Apaza"
+email = "davidguevaraapaza@gmail.com"
+username = "@davo"
+biostack = "Data Science & Genomics"
+
+
+println("Name: ", name)
+println("Email: ", email)
+println("Slack username: ", username)
+println("Biostack: ", biostack)
--- stage0_David.jl
+++
@@ -1,4 +0,0 @@
-########### HACKBIO 2021 ###########
-# Stage 0 - Introduction
-
-println("Name: David Guevara-Apaza\nEmail: davidguevaraapaza@gmail.com\nSlack username: @davo\nBiostack: Genomics\n")
