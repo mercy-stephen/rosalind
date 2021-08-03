51194da7a700af62cc50860de74d073b355c8c1d
rohanraj11322 <83224907+rohanraj11322@users.noreply.github.com>
2021-08-03 16:46:33 +0530

    Add files via upload

---
+++ Rohan_kumar.py
@@ -0,0 +1,12 @@
+ # HackBio Internship 2021 
+name= ("Name: Rohan Kumar")
+print (name)
+
+email = ("Email: rohancoolraj5@gmail.com")
+print (email)
+
+username = ("Slack Username: @Rohan")
+print (username)
+
+Biostacks = ("Biostacks: Genomics")
+print (Biostacks)
