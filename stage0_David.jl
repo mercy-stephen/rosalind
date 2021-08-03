8861ceafbd3d1b73564fef3887c1a982883d539b
yodavo <davidguevaraapaza@gmail.com>
2021-08-02 22:12:15 -0500

    David Intro in Julia Language

---
+++ stage0_David.jl
@@ -0,0 +1,4 @@
+########### HACKBIO 2021 ###########
+# Stage 0 - Introduction
+
+println("Name: David Guevara-Apaza\nEmail: davidguevaraapaza@gmail.com\nSlack username: @davo\nBiostack: Genomics\n")
