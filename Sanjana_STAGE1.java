343a37bd70ad40050b9b8ad8e10fd87749330b29
Sanjana Joshi <66120868+sanjana-joshi14@users.noreply.github.com>
2021-08-03 11:41:19 +0530

    Add files via upload

---
+++ Sanjana_STAGE1.java
@@ -0,0 +1,10 @@
+
+public class Sanjana_STAGE1 {
+    public static void main(String[] args) {   
+     System.out.println("Name: Sanjana Shailesh Joshi");
+     System.out.println("Email id: sanjanajo868@gmailcom");    
+     System.out.println("Slack username: @Sanjana1404");     
+     System.out.println("Biostack: Genomics");   
+    }
+}
+        
