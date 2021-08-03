439138ee14290c13d93aaf3f0c24727680ad1658
Bhavya Saini <75350171+bhavyasaini13@users.noreply.github.com>
2021-08-03 12:20:14 +0530

    Adding Bhavya Intro task using JAVASCRIPT
    Used javascript without HTML.

---
+++ intro.js
@@ -0,0 +1,7 @@
+// HACKBIO 2021
+// stage 0: Bhavya's Code
+
+console.log("Name: Bhavya Saini")
+console.log("Email: bhavyasaini804@gmail.com")
+console.log("Slack Username: Bhavya")
+console.log("Biostack: Transcriptomics, Data Science, Vaccine Informatics")
