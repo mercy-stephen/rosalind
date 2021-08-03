d2d05f7a45216b0433e67614de94f45a0c716b95
Nanthana Ravichandran <88301056+nanthanaravichandran@users.noreply.github.com>
2021-08-03 13:51:37 +0530

    Nanthana stage-0 in Python script

---
+++ Nanthana_stage0.py
@@ -0,0 +1,7 @@
+#HackBio Internship 2021
+#Nanthana's code to complete stage 0
+
+print("Name: Nanthana Ravichandran")
+print("Email ID: nanthanaravichandran@gmail.com")
+print("Slack username: @Nanthana")
+print("Biostack: Transcriptomics")
