c1ea36bdf20853d70bf4c574d1904c3961fb8854
Poornima <88300459+Poornimahackb@users.noreply.github.com>
2021-08-04 12:14:21 +0530

    Add files via upload

---
+++ test_repo.R
@@ -0,0 +1,7 @@
+#!usr/bin/poornima
+########### HACKBIO 2021 ###########
+# Stage 0 - Introduction
+print('name:  Poornima')
+print('email:  poornimasivamani@gmail.com')
+print('slack :  poornimacorp@slack.com')
+print('biostack:  genomics')
