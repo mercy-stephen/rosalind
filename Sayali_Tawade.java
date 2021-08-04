acf87475676231f44d1eb71e55e1bb19313f3367
Sayalitawade <88305946+Sayalitawade@users.noreply.github.com>
2021-08-04 14:19:15 +0530

    Update Sayali_Tawade.java

--- Sayali_Tawade.java
+++ Sayali_Tawade.java
@@ -5,5 +5,8 @@ public class Sayali {
      System.out.println("Email id: tawadesayali4848@gmail.com");    
      System.out.println("Slack username: @Sayali");     
      System.out.println("Biostack:Data science, Genomics");   
+     System.out.println("Twitter handle:@tawade_sayali");  
+      
+     
     }
 }
