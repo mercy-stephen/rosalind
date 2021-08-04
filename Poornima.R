ccdb3c2bbf6a237f7fe651ff950ca38ba46b40b3
Poornima <88300459+Poornimahackb@users.noreply.github.com>
2021-08-04 12:26:33 +0530

    Delete Poornima.R

--- Poornima.R
+++
@@ -1,7 +0,0 @@
-#!usr/bin/poornima
-########### HACKBIO 2021 ###########
-# Stage 0 - Introduction
-print('name:  Poornima')
-print('email:  poornimasivamani@gmail.com')
-print('slack :  poornimacorp@slack.com')
-print('biostack:  genomics')
