56f956afd36d22d130ae62b6263d8b8dd1a3346f
Aditisingh11322 <84804618+Aditisingh11322@users.noreply.github.com>
2021-08-04 19:06:09 +0530

    Update Aditi_Singh.py

--- Aditi_Singh.py
+++ Aditi_Singh.py
@@ -1,5 +1,25 @@
  # HackBio Internship 2021 
 print("Name: Aditi Singh")
 print("Email: singhaditi7733@gmail.com")
-print("Slack Username: @Aditi")
+print("Slack Username: @AditiSingh12345")
+
+Twitter = ("Twitter username: @@AditiSi28068977")
+print (Twitter)
 print("Biostacks: Genomics")
+
+
+def hammingDistance(slack, twitter):
+    i = 0
+    count = 0
+ 
+    while(i < len(slack)):
+        if(slack[i] != twitter[i]):
+            count += 1
+        i += 1
+    return count
+ 
+
+slack = "AditiSingh12345"
+twitter = "AditiSi28068977"
+ 
+print(hammingDistance(slack, twitter))
