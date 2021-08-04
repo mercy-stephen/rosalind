4227a9e5bec95b8c9ee4a6576c1a218868288b74
Poornima <88300459+Poornimahackb@users.noreply.github.com>
2021-08-04 16:19:51 +0530

    Add files via upload

---
+++ test_repo.R.R
@@ -0,0 +1,24 @@
+#!usr/bin/R
+########### HACKBIO 2021 ###########
+# Stage 0 - Introduction
+print('name:  Poornima')
+print('email:  poornimasivamani@gmail.com')
+print('slack :  poornimacorp@slack.com')
+print('biostack:  genomics')
+
+
+#create vector
+print('twitter handlle-poornim@41024410')
+
+#find hamming distance
+
+print('x-slack handle')
+print('y-twitter handle')
+x <- print('p,o,o,r,n,i,m,a')
+y <- print('p,o,o,r,n,i,m,@,4,1,0,2,4,4,1,0')
+
+
+print('find hamming distance between x and y')
+
+sum(x!=y)
+print('[1] 1')
