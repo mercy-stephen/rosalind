9afcf7560aff4a458721d542fa6b2e6974c51ec3
Bhavya Saini <75350171+bhavyasaini13@users.noreply.github.com>
2021-08-04 12:48:03 +0530

    Update bhavya_intro.js

--- bhavya_intro.js
+++ bhavya_intro.js
@@ -5,3 +5,20 @@ console.log("Name: Bhavya Saini");
 console.log("Email: bhavyasaini804@gmail.com");
 console.log("Slack Username: Bhavya");
 console.log("Biostack: Transcriptomics, Data Science, Vaccine Informatics");
+console.log("Twitter: @bhavyeah13");
+
+const str1 = 'Bhavya';
+const str2 = 'bhavyeah13';
+const findHammingDistance = (str1 = '', str2 = '') => {
+   let distance = 0;
+   if(str1.length === str2.length) {
+      for (let i = 0; i < str1.length; i++) {
+         if (str1[i].toLowerCase() != str2[i].toLowerCase()){
+            distance++
+         }
+      }
+      return distance
+   };
+   return 0;
+};
+console.log("Hamming Distance:", findHammingDistance(str1, str2));
