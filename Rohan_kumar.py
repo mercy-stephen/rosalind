eee95fd800256f38befc045a737eef61ef7ddd63
Rohan Kumar <83224907+rohanraj11322@users.noreply.github.com>
2021-08-04 19:06:08 +0530

    Update Rohan_kumar.py

--- Rohan_kumar.py
+++ Rohan_kumar.py
@@ -5,8 +5,27 @@ print (name)
 email = ("Email: rohancoolraj5@gmail.com")
 print (email)
 
-username = ("Slack Username: @Rohan")
+username = ("Slack Username: @Rohanraj11")
 print (username)
+
+Twitter = ("Twitter username: @Rohan11322")
+print (Twitter)
 
 Biostacks = ("Biostacks: Genomics")
 print (Biostacks)
+
+def hammingDistance(slack, twitter):
+    i = 0
+    count = 0
+ 
+    while(i < len(slack)):
+        if(slack[i] != twitter[i]):
+            count += 1
+        i += 1
+    return count
+ 
+
+slack = "Rohanraj11"
+twitter = "Rohan11322"
+ 
+print(hammingDistance(slack, twitter))
