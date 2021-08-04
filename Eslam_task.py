5bd57b0a2c15dfa81cbab44c8209c1d68e1f13f4
EslamShedid55520 <88290865+EslamShedid55520@users.noreply.github.com>
2021-08-04 22:35:06 +0200

    Add files via upload

---
+++ Eslam_task.py
@@ -0,0 +1,13 @@
+Data={"Name: " :"Eslam Mohammed Shedid", "Email: ":"Eslamshedid55520@gmail.com" , "slack_username: ":"@Eslam55520" , "biostack: ": "Genomics" , "twitter_handle: " : "@Eslam15112"  }
+counter=0
+for a in Data:
+    print a + Data[a]
+s1=Data["slack_username: "]
+s2=Data["twitter_handle: "]
+for x in range(len(s1)):
+    if s1[x]==s2[x]:
+        continue
+    else:
+        counter+=1
+print "hamming_distance: " +`counter` 
+x=input("if you want to end the program press enter")
