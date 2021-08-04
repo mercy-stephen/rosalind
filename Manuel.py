8e1ae4c03d93d4045b04056358ba52b8d16f6e4e
Emmanuel Ani <88311290+Manuellzz@users.noreply.github.com>
2021-08-04 12:53:32 +0100

    Add files via upload

---
+++ Manuel.py
@@ -0,0 +1,11 @@
+name ='Emmanuel Ani'
+email = 'emmanuelani234@gmail.com'
+slack_username = '@Manuel'
+biostack ='Data Science'
+twitter_handle ='emmanuelani234'
+
+print(name)
+print(email) 
+print(slack_username)
+print(biostack)
+print(twitter_handle)
