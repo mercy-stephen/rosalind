c91471873151177ae6ed51f5f52a2d15f02ea544
Nanthana Ravichandran <88301056+nanthanaravichandran@users.noreply.github.com>
2021-08-04 17:50:54 +0530

    Delete Nanthana_stage0.py

--- Nanthana_stage0.py
+++
@@ -1,7 +0,0 @@
-#HackBio Internship 2021
-#Nanthana's code to complete stage 0
-
-print("Name: Nanthana Ravichandran")
-print("Email ID: nanthanaravichandran@gmail.com")
-print("Slack username: @Nanthana")
-print("Biostack: Transcriptomics")
