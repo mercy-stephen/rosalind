35ea0950f9d367baeabd2bed8c0d34055920f9c7
Sayalitawade <88305946+Sayalitawade@users.noreply.github.com>
2021-08-05 19:20:54 +0530

    Update Sayali_Tawade.java

--- Sayali_Tawade.java
+++ Sayali_Tawade.java
@@ -1,12 +1,5 @@
 #!usr/bin/java
 public class Sayali_tawade{
-    public static void main(String[] args) {   
-     System.out.println("Name: Sayali Jayendra Tawade");
-     System.out.println("Email id: tawadesayali4848@gmail.com");    
-     System.out.println("Slack username: @Sayali");     
-     System.out.println("Biostack:Data science, Genomics");   
-     System.out.println("Twitter handle:@tawade_sayali");  
-    }
 
     public static int getDistance(String x, String y){
 
@@ -25,7 +18,13 @@ public class Sayali_tawade{
         System.out.println("x="+x+", y="+y+"  Hamming distance: " + humming_distance);
 
     }
-
+public static void main(String[] args) {   
+     System.out.println("Name: Sayali Jayendra Tawade");
+     System.out.println("Email id: tawadesayali4848@gmail.com");    
+     System.out.println("Slack username: @Sayali");     
+     System.out.println("Biostack:Data science, Genomics");   
+     System.out.println("Twitter handle:@tawade_sayali");  
+    
         String x = "Sayali";
         String y = "tawade_sayali";
         getDistance(x, y);
