cb6c609c190ec2a6daeb073048a45d065327e0b6
Emmanuel Ani <88311290+Manuellzz@users.noreply.github.com>
2021-08-05 12:57:51 +0100

    Manuel hamming distance update

--- Manuel.py
+++ Manuel.py
@@ -1,11 +1,22 @@
 name ='Emmanuel Ani'
 email = 'emmanuelani234@gmail.com'
 slack_username = '@Manuel'
-biostack ='Data Science'
+biostack = 'Data Science'
 twitter_handle ='emmanuelani234'
+
+def hammingDistance(string1, string2):
+    i = 0
+    count = 0
+        while i < len(string1):
+            if (string1[i] != string2[i]):
+                count += 1
+            i += 1
+        return count
 
 print(name)
 print(email) 
 print(slack_username)
 print(biostack)
 print(twitter_handle)
+
+hammingDistance(slack, twitter_handle)
