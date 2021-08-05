c0dc9b43ec0014e95dfe37adaf982e6d62a61912
Vanshika Agarwal <72395964+Vanshika-agarwal05@users.noreply.github.com>
2021-08-05 13:34:56 +0530

    Update Vanshika_intro.c

--- Vanshika_intro.c
+++ Vanshika_intro.c
@@ -1,13 +1,15 @@
-﻿#include <stdio.h>
+//#!/user/bin/c
+
+#include <stdio.h>
 #include <string.h>
 int main() {
             int i, length, count = 0;
             printf("\n Name : Vanshika Agarwal ") ;
             printf("\n Email : vanshika.agarwal98@gmail.com ");  
-            printf("\n slack Username : @Vanshika160 ") ;
+            printf("\n slack Username : @Vanshika_16") ;
             printf("\n Biostack : Data Science,Vaccine Informatics, Genomics") ;
             printf("\n Twitter : @Vanshika165 ") ;
-            char S1[] = "Vanshika160" ;
+            char S1[] = "Vanshika_16" ;
             char S2[]="Vanshika165" ;
             length = strlen(S2);
             for(i = 0; i < length; i++)
