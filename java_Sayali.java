be53dce9b8a777f7462d76afa63374efca24d3f9
Sayalitawade <88305946+Sayalitawade@users.noreply.github.com>
2021-08-06 16:59:24 +0530

    Update java_Sayali.java

--- java_Sayali.java
+++ java_Sayali.java
@@ -15,7 +15,7 @@ public static void main(String[] args) {
      System.out.println("Name: Sayali Jayendra Tawade");
      System.out.println("Email id: tawadesayali4848@gmail.com");    
      System.out.println("Slack username: @Sayali");     
-     System.out.println("Biostack:Data science, Genomics");   
+     System.out.println("Biostack:Data science & Genomics");   
      System.out.println("Twitter handle:@tawade_sayali");  
     
         String x = "Sayali";
