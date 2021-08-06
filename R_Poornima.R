6a36d744d5be4c875ffe9a7dbeed67b5e697fdaf
mercy-stephen <81503326+mercy-stephen@users.noreply.github.com>
2021-08-06 22:55:44 +0530

    Update R_Poornima.R

--- R_Poornima.R
+++ R_Poornima.R
@@ -1,24 +1,24 @@
 #!usr/bin/R
 ########### HACKBIO 2021 ###########
 # Stage 0 - Introduction
-print('name:  Poornima')
-print('email:  poornimasivamani@gmail.com')
-print('slack :  poornimacorp@slack.com')
-print('biostack:  genomics')
+print('Name:Poornima')
+print('Email:poornimasivamani@gmail.com')
+print('<Slack_username>:poornimacorp@slack.com')
+print('Biostack:genomics')
 
 
 #create vector
-print('twitter handlle-poornim@41024410')
+print('<Twitter_handlle>:poornim@41024410')
 
 #find hamming distance
 
-print('x-slack ID')
-print('y-twitter handle')
+print('x-<Slack_username>')
+print('y-<Twitter_handlle>')
 x <- print('p,o,o,r,n,i,m,a')
 y <- print('p,o,o,r,n,i,m,a')
 
 
-print('find hamming distance between x and y')
+print('<Hamming_Distance>')
 
 sum(x!=y)
 returnValue('0')
