d7b2c4d4b7c6099d02298a1f1d9b5b064f0769a0
Mohamed Said Ahmed AboHoussien <48734941+medo01025@users.noreply.github.com>
2021-08-06 03:02:07 +0200

    My first solution

---
+++ Mohamed AboHoussien Task 1.R
@@ -0,0 +1,10 @@
+my_name ="My Name is (Mohamed)"
+my_email="My E-mail is 'mohamedsaidabo@gmail.com'"
+my_biostack="My Biostack is (Genomics and metagenomics)"
+my_first_assignment_solution =c(my_name="Mohamed",my_email="mohamedsaidabo@gmail.com"
+      ,my_slack_username="@Mohamed1852467",my_twitter_handle="@Mohamed0102522"
+      ,my_biostack="Genomics and metagenomics",Hamming_distance =
+        stringdist(my_slack_username, my_twitter_handle, 
+                   method="hamming"))
+my_first_assignment_solution
+View(my_first_assignment_solution)
