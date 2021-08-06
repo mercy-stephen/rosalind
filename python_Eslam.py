b6f9ea7fd96e872dc795282eaa7b0b19dcc9276c
EslamShedid55520 <88290865+EslamShedid55520@users.noreply.github.com>
2021-08-06 19:24:29 +0200

    Update python_Eslam.py

--- python_Eslam.py
+++ python_Eslam.py
@@ -1,11 +1,10 @@
-Data={"Name:":"Eslam Mohammed Shedid", "Email:":"Eslamshedid55520@gmail.com" , "<Slack_username>:":"@Eslam55520" , "Biostack:":"Genomics" , "twitter_handle:":"@Eslam15112"  }
+Data={"Name:":"Eslam Mohammed Shedid", "Email:":"Eslamshedid55520@gmail.com" , "<Slack_username>:":"@Eslam55520" , "Biostack:":"Genomics" , "<Twitter_handle>:":"@Eslam15112"  }
 counter=0
 for a in Data:
     print a + Data[a]
-s1=Data["<slack_username:>"]
-s2=Data["<twitter_handle:>"]
+s1=Data["<Slack_username>:"]
+s2=Data["<Twitter_handle>:"]
 for x in range(len(s1)):
     if s1[x]!=s2[x]:
         counter+=1
-print "Hamming_Distance:" +`counter` 
-x=input("if you want to end the program press enter")
+print "<Hamming_Distance>:" +`counter` 
