72d11ba10a3f5b07ca98cff021cbcd77708da345
mercy-stephen <81503326+mercy-stephen@users.noreply.github.com>
2021-08-06 23:02:11 +0530

    Update R_Mohamed.R

--- R_Mohamed.R
+++ R_Mohamed.R
@@ -1,10 +1,10 @@
-my_name ="My Name is (Mohamed)"
-my_email="My E-mail is 'mohamedsaidabo@gmail.com'"
-my_biostack="My Biostack is (Genomics and metagenomics)"
-my_first_assignment_solution =c(my_name="Mohamed",my_email="mohamedsaidabo@gmail.com"
-      ,my_slack_username="@Mohamed1852467",my_twitter_handle="@Mohamed0102522"
-      ,my_biostack="Genomics and metagenomics",Hamming_distance =
-        stringdist(my_slack_username, my_twitter_handle, 
+Name="My Name is (Mohamed)"
+Email="My E-mail is 'mohamedsaidabo@gmail.com'"
+Biostack="My Biostack is (Genomics and metagenomics)"
+my_first_assignment_solution =c(Name="Mohamed",Email="mohamedsaidabo@gmail.com"
+      ,<slack_username>="@Mohamed1852467",<Twitter_handle>="@Mohamed0102522"
+      ,Biostack="Genomics and metagenomics",<Hamming_distance>=
+        stringdist(<Slack_username>, <twitter_handle>, 
                    method="hamming"))
 my_first_assignment_solution
 View(my_first_assignment_solution)
