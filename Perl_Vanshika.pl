d811fc6259f54d64edee8b3341e79150083df8ca
Vanshika Agarwal <72395964+Vanshika-agarwal05@users.noreply.github.com>
2021-08-08 21:50:24 +0530

    Create Perl_Vanshika.pl

---
+++ Perl_Vanshika.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+ 
+$Name= 'Vanshika Agarwal ';
+$email= 'vanshika.agarwal98@gmail.com';
+$Slack_id = '@Vanshika_16';
+$Biostack = 'Data Science&Vaccineinformatics&Genomic ';
+$twitter= '@Vanshika165';
+ 
+print  ("Name:$Name\n") ;
+print ("Email :$email\n") ;
+print  ("Slack_id:$Slack_id\n") ;
+print ("Biostack:$Biostack\n") ;
+print ("Twitter:$twitter\n"); 
+$x="Vanshika_16";
+$y= "Vanshika165";
+$ham_dist=0;
+for (my $i=0; $i<length $y; $i++)
+     {
+      ++$ham_dist if substr($y, $i, 1) ne substr($x, $i, 1);
+     }
+print(" Hamming Distance :$ham_dist" ) 
