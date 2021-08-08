3497f7f01757db4cd12e03144cae16309527c190
mercy-stephen <81503326+mercy-stephen@users.noreply.github.com>
2021-08-08 19:10:10 +0530

    Final

--- gcc_Vanshika.c
+++ gcc_Vanshika.c
@@ -4,10 +4,10 @@
 #include <string.h>
 int main() {
             int i, length, count = 0;
-            printf("\nName:Vanshika Agarwal ") ;
-            printf("\nEmail:vanshika.agarwal98@gmail.com ");  
+            printf("\nName:Vanshika Agarwal") ;
+            printf("\nEmail:vanshika.agarwal98@gmail.com");  
             printf("\n<Slack_Username>:@Vanshika_16") ;
-            printf("\n<Twitter_handle>:@Vanshika165 ") ;
+            printf("\n<Twitter_handle>:@Vanshika165") ;
             printf("\nBiostack:Data Science & Vaccine Informatics") ;
             char S1[] = "Vanshika_16" ;
             char S2[]="Vanshika165" ;
