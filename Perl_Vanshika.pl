c3e08a3406f834100e7cacc8b3e6e5909e128c36
mercy-stephen <81503326+mercy-stephen@users.noreply.github.com>
2021-08-08 21:56:07 +0530

    final

--- Perl_Vanshika.pl
+++ Perl_Vanshika.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/perl
  
-$Name= 'Vanshika Agarwal ';
+$Name= 'Vanshika Agarwal';
 $email= 'vanshika.agarwal98@gmail.com';
 $Slack_id = '@Vanshika_16';
-$Biostack = 'Data Science&Vaccineinformatics&Genomic ';
+$Biostack = 'Data Science&Vaccineinformatics&Genomic';
 $twitter= '@Vanshika165';
  
 print  ("Name:$Name\n") ;
 print ("Email :$email\n") ;
-print  ("Slack_id:$Slack_id\n") ;
+print  ("<Slack_username>:$Slack_id\n") ;
 print ("Biostack:$Biostack\n") ;
-print ("Twitter:$twitter\n"); 
+print ("<Twitter_handle>:$twitter\n"); 
 $x="Vanshika_16";
 $y= "Vanshika165";
 $ham_dist=0;
@@ -18,4 +18,4 @@ for (my $i=0; $i<length $y; $i++)
      {
       ++$ham_dist if substr($y, $i, 1) ne substr($x, $i, 1);
      }
-print(" Hamming Distance :$ham_dist" ) 
+print("<Hamming_Distance>:$ham_dist" ) 
